f3ef36f46eda4979cdc4e444fd7a3464db99baed
傲娇小霖霖 <1589861957@qq.com>
2024-12-23 22:41:38 +0800

    add turbo init

--- apps/you-gao-tou/src/App.vue
+++ apps/you-gao-tou/src/App.vue
@@ -5,10 +5,19 @@ import { getGameCountApi } from '#/api';
 getGameCountApi()
 const userInfo = wxLoginGetUserInfo("KXLZAQPCsU7zhpLXVgUFstfqEXR6BS%2BaYUT7Mks0EJAG71njSF%2BQH7HJFi8P/r4K9%2B1HWfrGICm5ryCEoyv7NLjQiUW%2BkdTXOseXLgq5m7eAdquxBN6tIzF1X4mYBTt6JGlamiESsFG6%2BM/JKLXvRkpcRpByYevZaBBy3ffH0RHIAfOmHNMHA5MXwPHx0ioDa2G7ABtKHmkYZJz4XLrkpeYTazTCZhOS/RmeqO1BkrUEWWW%2BSK5sl2gu3GvP/HPHK8Dhcvf/xrVr0ePn7rHB7YmRnrMuzWzewOB6hs4l09/v77hFInW1sSyrhG3A0RlKq%2BmI1VwGl2rw7nJyynA10rBiyQWP0HM2/uRP5IiyOQY=")
 console.log(userInfo)
+const count = ref(0)
 </script>
 
 <template>
   <div>
     <h1>四川方言</h1>
+    <article>
+      {{ userInfo }}
+    </article>
+    <div>
+      <h2>游戏次数</h2>
+      <p>{{ count }}=={{ count * 10 }}</p>
+      <button @click="count++">增加</button>
+    </div>
   </div>
 </template>
