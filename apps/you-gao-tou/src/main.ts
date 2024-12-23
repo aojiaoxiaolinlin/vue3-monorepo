9f2892ed1699052fedc41dcbbe1cf6aa1b9606cc
傲娇小霖霖 <1589861957@qq.com>
2024-12-23 23:24:17 +0800

    使用catalog管理依赖

--- apps/you-gao-tou/src/main.ts
+++ apps/you-gao-tou/src/main.ts
@@ -1,13 +1,11 @@
-import { Search } from 'vant';
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import './style.css'
-import 'vant/lib/index.css';
 // 适配移动端的配置
 import 'amfe-flexible';
 // 禁用开发者工具
 import { disableDevtool } from '../configs';
 disableDevtool();
 
-createApp(App).use(Search).mount('#app')
+createApp(App).mount('#app')
