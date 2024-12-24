d8cf494bbbaa0c2c19b5f2fc3433321124cf427c
傲娇小霖霖 <1589861957@qq.com>
2024-12-24 20:37:26 +0800

    Revert "使用catalog管理依赖"
    This reverts commit 9f2892ed1699052fedc41dcbbe1cf6aa1b9606cc.

--- apps/you-gao-tou/src/main.ts
+++ apps/you-gao-tou/src/main.ts
@@ -1,11 +1,13 @@
+import { Search } from 'vant';
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import './style.css'
+import 'vant/lib/index.css';
 // 适配移动端的配置
 import 'amfe-flexible';
 // 禁用开发者工具
 import { disableDevtool } from '../configs';
 disableDevtool();
 
-createApp(App).mount('#app')
+createApp(App).use(Search).mount('#app')
